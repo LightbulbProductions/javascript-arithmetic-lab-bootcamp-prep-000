00bbcfa4cf3323ff0fc1a0b8f19f763fb4b51701
Michelle G <gerbermichelle23@gmail.com>
2019-03-22 13:55:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-1 + 80 // 81
-60 - 40 // 20
-2 * 3.4 // 6.8 
-5.0 / 2.5 // 2
+//1 + 80 // 81
+//60 - 40 // 20
+//2 * 3.4 // 6.8 
+//5.0 / 2.5 // 2
 var number;
-function add(a, b){ //add a and b and return the result.
+function add(a, b){ 
   number = a + b;
   return number;
 }
 var number;
-function subtract(a, b){ //add a and b and return the result.
+function subtract(a, b){  
   number = a - b;
   return number;
 }
 
+function multiply(a, b){  
+  number = a * b;
+  return number;
+}
+function divide(a, b){
+  number = a / b;
+  return number;
+}
+function inc(a){
+  number = a + 1;
+  return number;
+}
+function dec(a){ 
+  number = a - 1;
+  return number;
+}
+function makeInt(a){  
+  number = parseInt(a, 10);
+  return number;
+}
