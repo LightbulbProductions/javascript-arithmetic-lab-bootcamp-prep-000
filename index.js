145f8742fbb3d47b4d1df0e549ca1b7d827947f5
Michelle G <gerbermichelle23@gmail.com>
2019-03-22 13:51:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,14 @@
 60 - 40 // 20
 2 * 3.4 // 6.8 
 5.0 / 2.5 // 2
+var number;
+function add(a, b){ //add a and b and return the result.
+  number = a + b;
+  return number;
+}
+var number;
+function subtract(a, b){ //add a and b and return the result.
+  number = a - b;
+  return number;
+}
+
